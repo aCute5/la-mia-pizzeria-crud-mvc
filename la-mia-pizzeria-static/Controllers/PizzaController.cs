8ea5986dd723c33c660bbaae793001f8f702273d
Dario Costantini <costantini95dario@gmail.com>
2023-04-12 16:46:33 +0200

    trying adding details and delete

--- la-mia-pizzeria-static/Controllers/PizzaController.cs
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -53,6 +53,43 @@ namespace la_mia_pizzeria_static.Controllers
         
         
         }
+        [HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Update(int id)
+		{
+			using (var ctx = new PizzaContext())
+            {
+              var pizza = ctx.Pizze.FirstOrDefault(x => x.Id == id);
+                if (pizza == null)
+                {
+                    return View("Not found");
+                }
+                else
+                {
+                    return View(pizza); 
+                }
+            }
+			
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Update(int id, PizzaModel pizza)
+        {
+          
+            using (PizzaContext ctx = new PizzaContext())
+            {
+                PizzaModel pizzatoEdit = ctx.Pizze.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+                if(pizzatoEdit == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View("Edit",pizzatoEdit);
+                }
+            }
+        }
     }
 
 }
